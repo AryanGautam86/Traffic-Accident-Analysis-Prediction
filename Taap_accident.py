81438c530f6df1c183594b8b6b82845e40344df0
Aryan Gautam <aryannsamraat086@gmail.com>
2025-09-06 19:31:42 +0530

    Update Taap_accident.py

--- Taap_accident.py
+++ Taap_accident.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-# 2. Class which describes Bank Notes measurements
+# 2. Class which describes features of Taap
 class Taap_acc(BaseModel):
     Hour: int
     Day: int 
