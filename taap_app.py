8a360914875716d08608a71bb102b7551d7a36b5
Aryan Gautam <aryannsamraat086@gmail.com>
2025-09-06 19:20:26 +0530

    Add files via upload

---
+++ taap_app.py
@@ -0,0 +1,27 @@
+# main.py
+import uvicorn
+from fastapi import FastAPI
+from Taap_accident import Taap_acc
+import joblib
+import numpy as np
+import pandas as pd
+
+# Load the model
+model = joblib.load("t_accident_pred.joblib")
+
+# Initialize app
+taap_app = FastAPI()
+
+# Home route
+@taap_app.get("/")
+def read_root():
+    return {"message": "Model API is running!"}
+
+# Prediction route
+@taap_app.post("/predict")
+def predict(data: Taap_acc):
+    features = pd.DataFrame([{"Hour":data.Hour, "Day":data.Day, "LatBin":data.LatBin, "LonBin":data.LonBin}])
+    prediction = model.predict(features)
+    return {"prediction": int(prediction[0])}
+# if __name__ == '__main__':
+#     uvicorn.run(taap_app, host='127.0.0.1', port=8000,reload=True)
